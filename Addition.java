e713672fcef1a26bbf0ac1de3bf7672504fd0a0f
JYOTI <jyotiprakash@gmail.com>
2023-06-08 18:34:28 +0530

    addition

--- Addition.java
+++ Addition.java
@@ -1,5 +1,8 @@
-public class Addtion {
-	public static void main(String args[]) {
-		System.out.println(10 + 20);
+public class Addition {
+	public static void main(String args) {
+		int a = 10;
+		int b = 20;
+		int c = a+b;
+		System.out.println(c);
 	}
 }
